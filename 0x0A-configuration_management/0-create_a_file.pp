3ba86bdc4090444a761fc6d5ac6bea38a8f579fc
Ammar Khaled Abdel Nour Ibrahim Ali <111376558+Ammar-Khaled@users.noreply.github.com>
2023-08-25 14:24:38 +0300

    Create 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Creates a file in /tmp
+
+file { '/tmp/school'
+  path    => '/tmp/school',
+  ensure  => file,
+  mode    => 0744,
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
