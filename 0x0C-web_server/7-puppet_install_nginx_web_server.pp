b342deb0abfe4f55ea6d1ddcc9c4cd09699c0ca0
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-29 11:58:11 +0000

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,5 @@
 
 exec { 'install':
   provider => shell,
-  command  => 'apt-get -y update ; apt-get -y install nginx ; echo 'Hello World!' | sudo tee /var/www/html/index.nginx-debian.html ; sudo echo 'Ceci n\'est pas une page' | sudo tee /var/www/html/404.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Ammar-Khaled permanent;\n\n\terror_page 404 \/404.html;\n\tlocation = \/404.html {\n\t\troot \/var\/www\/html;\n\t\tinternal;\n\t}/" /etc/nginx/sites-available/default ; sudo service nginx start '
+  command  => 'apt-get -y update ; apt-get -y install nginx ; echo 'Hello World!' | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Ammar-Khaled permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start '
 }
