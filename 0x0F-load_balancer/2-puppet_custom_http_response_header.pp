970f36d7e514669dc432f25da44c1a84f994befe
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-09-04 15:17:17 +0000

    fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # Configure Nginx so that its HTTP response contains a custom header
 
 exec { 'install_and_conf_nginx':
-  provider => shell
+  provider => shell,
   command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; sudo echo "Holberton School" > /var/www/html/index.nginx-debian.html ; sudo echo "Ceci n\'est pas une page" > /usr/share/nginx/html/custom_404.html ; sudo sed -i "s/server_name _;/server_name _;\n\n\trewrite ^\/redirect_me https:\/\/github.com\/Ammar-Khaled permanent;\n\n\terror_page 404 \/custom_404.html;\n\tlocation = \/custom_404.html {\n\t\troot \/usr\/share\/nginx\/html;\n\t\tinternal;\n\t}/" /etc/nginx/sites-available/default ; sudo sed -i "s/server_name _;/server_name _;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/sites-available/default ; sudo service nginx start'
 }
