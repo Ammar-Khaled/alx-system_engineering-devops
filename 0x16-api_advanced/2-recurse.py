ba6341d53f1817ab72f64476ba43a09de4a600cf
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-10-10 17:42:31 +0000

    fixx

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,34 +1,44 @@
 #!/usr/bin/python3
-"""Return a list of titles of all hot articles for a given subreddit."""
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
 
 
 def add_title(hot_list, hot_posts):
-    """Add the titles of hot_posts to hot_list."""
+    """ Adds item into a list """
     if len(hot_posts) == 0:
         return
-
     hot_list.append(hot_posts[0]['data']['title'])
     hot_posts.pop(0)
     add_title(hot_list, hot_posts)
 
-def recurse(subreddit, hot_list=[], after=None):
-    """Print the titles of all hot posts for a given subreddit."""
-    r = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json",
-                     headers={'User-Agent': 'Mozilla/5.0'},
-                     params={'after': after},
-                     allow_redirects=False)
 
-    if (r.status_code != 200):
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    params = {
+        'after': after
+    }
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
+                       allow_redirects=False)
+
+    if res.status_code != 200:
         return None
 
-    dict = r.json()
-    hot_posts = dict['data']['children']
-
+    dic = res.json()
+    hot_posts = dic['data']['children']
     add_title(hot_list, hot_posts)
-
-    after = dict['data']['after']
+    after = dic['data']['after']
     if not after:
         return hot_list
-
     return recurse(subreddit, hot_list=hot_list, after=after)
