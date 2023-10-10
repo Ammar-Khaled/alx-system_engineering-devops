9b2384fc37d20c02beef1cfc7d80b078d8d4e724
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-10-10 15:04:57 +0000

    Added documentation for the modules

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Return the number of subscribers for a given subreddit."""
 import requests
 
 
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Print the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
