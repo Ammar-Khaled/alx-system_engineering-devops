54767ad030f289df1eacd0ec5f07ac198e5b0dfb
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-10-10 22:05:04 +0000

    try#6

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""Print the titles of posts of a subreddit."""
+"""
+Function that queries the Reddit API and prints
+the top ten hot posts of a subreddit
+"""
 import requests
+import sys
 
 
 def add_title(hot_list, hot_posts):
-    """Add item into a list."""
+    """ Adds item into a list """
     if len(hot_posts) == 0:
         return
     hot_list.append(hot_posts[0]['data']['title'])
@@ -13,7 +17,7 @@ def add_title(hot_list, hot_posts):
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """Query to Reddit API."""
+    """ Queries to Reddit API """
     u_agent = 'Mozilla/5.0'
     headers = {
         'User-Agent': u_agent
@@ -33,7 +37,6 @@ def recurse(subreddit, hot_list=[], after=None):
         return None
 
     dic = res.json()
-
     hot_posts = dic['data']['children']
     add_title(hot_list, hot_posts)
     after = dic['data']['after']
