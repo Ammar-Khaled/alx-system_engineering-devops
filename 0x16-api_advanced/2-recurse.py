0b18fcc3967cb004cab11c52228a785d5cda06bd
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-10-10 16:53:15 +0000

    Added task#2

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Return a list containing the titles of all hot articles
+for a given subreddit.
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """Print the titles of all hot posts for a given subreddit."""
+
+    r = requests.get(f"https://www.reddit.com/r/{subreddit}/hot.json",
+                     headers={'User-Agent': 'Mozilla/5.0'},
+                     params={'after': after},
+                     allow_redirects=False)
+
+    if (r.status_code != 200):
+        return None
+
+    dict = r.json()
+    if 'data' not in dict.keys():
+        return None
+
+    if 'children' not in dict['data'].keys():
+        return None
+    hot_posts = dict['data']['children']
+
+    for post in hot_posts:
+        hot_list.append(post['data']['title'])
+
+    after = dict['data']['after']
+    if after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, after=after)
